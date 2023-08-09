53cc633c8320401dcb3e4d43db23e102777bcb93
ZLY <ZLY@zly.com>
2023-08-09 09:32:10 +0800

    push commit

--- src/main/java/com/zly/git/GitTest.java
+++ src/main/java/com/zly/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello gitlab!!!");
         System.out.println("hello git!!!");
         System.out.println("hello gitlab!!!");
+        System.out.println("hello github!!!");
     }
 }
